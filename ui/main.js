f355d47a7c6d13ef006cd01050da92dc593e838d
KshitijSrivastava <kshitij127@yahoo.co.in>
2016-10-11 23:43:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,31 @@
-console.log('Loaded!');
+//console.log('Loaded!');
 //changes the text of main-text div
-var element= document.getElementById("main-text");
-element.innerHTML="New Value";
+//var element= document.getElementById("main-text");
+//element.innerHTML="New Value";
 
 //move the image
-var img=document.getElementById("madi");
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+"px";
-}
+//var img=document.getElementById("madi");
+//var marginLeft=0;
+//function moveRight(){
+  //  marginLeft=marginLeft+1;
+    //img.style.marginLeft=marginLeft+"px";
+//}
 
-img.onclick=function() {
-    var interval=setInterval(moveRight,50);
+//img.onclick=function() {
+  //  var interval=setInterval(moveRight,50);
+//}
+
+//counter code
+var element=document.getElementById("counter"); 
+var counter=0;
+
+element.onclick=function(){
+    //make a request to counter endpoint
+    
+    //capture response and store it
+    
+    //render the variable in correct span
+    counter=counter+1;
+    var span=document.getElementById("span");
+    span.innerHTML=counter.toString();
 }
