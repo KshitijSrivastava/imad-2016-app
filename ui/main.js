099abe385c4f60212507812339721159a980f875
KshitijSrivastava <kshitij127@yahoo.co.in>
2016-10-11 16:57:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ element.innerHTML="New Value";
 
 //move the image
 var img=document.getElementById("madi");
-
+var marginLeft=0;
 function moveRight(){
     marginLeft=marginLeft+1;
     img.style.marginLeft=marginLeft+"px";
