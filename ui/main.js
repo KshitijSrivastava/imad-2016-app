42e176172344c62471017ef32e6420d4323d7422
KshitijSrivastava <kshitij127@yahoo.co.in>
2016-10-11 16:20:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 //changes the text of main-text div
 var element= document.getElementById("main-text");
 element.innerHTML="New Value";
+
+//move the image
+var img=document.getElementById("madi");
+img.onclick=function() {
+    img.style.marginLeft="100px";
+}
