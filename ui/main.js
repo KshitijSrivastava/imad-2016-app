6002747ac2f43c02066a6e4a1d9b8f58ad291956
KshitijSrivastava <kshitij127@yahoo.co.in>
2016-10-12 12:53:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -39,8 +39,7 @@ button.onclick=function(){
     request.send(null);
 };
 
-var nameInput=document.getElementById("name");
-var name=nameInput.value;
+
 var submit=document.getElementById("submit_btn");
 
 submit.onclick=function(){
@@ -67,6 +66,8 @@ submit.onclick=function(){
         }
         // not done yet
     };
+    var nameInput=document.getElementById("name");
+    var name=nameInput.value;
     //Make a request
     request.open("GET","http://kshitijsrivastava.imad.hasura-app.io/submit-name?name="+ name,true);
     request.send(null);
