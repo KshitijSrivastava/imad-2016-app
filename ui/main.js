cab9f4c684014ea0c360b596e79b2660e344bacd
KshitijSrivastava <kshitij127@yahoo.co.in>
2016-10-12 12:49:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -45,16 +45,33 @@ var submit=document.getElementById("submit_btn");
 
 submit.onclick=function(){
     // make a request to the server and send the name
+   //create a request object
+    var request= new XMLHttpRequest();
     
-    //capture a list of names and render as a list 
-    var names=["name1","name2","name3","name4"];
-    var list="";
-    for(var i=0;i<names.length;i++)
-    {
-        list+="<li>"+names[i]+"</li>";
-    }
-    var ul=document.getElementById("namelist");
-    ul.innerHTML=list;
+    //capture response and store it
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            //take some action
+            if(request.status===200){
+                //capture a list of names and render as a list 
+               var names=request.responseText;
+               names=JSON.parse(names);
+                var list="";
+                for(var i=0;i<names.length;i++)
+                {
+                 list+="<li>"+names[i]+"</li>";
+                }
+                 var ul=document.getElementById("namelist");
+                ul.innerHTML=list;
+            }
+        }
+        // not done yet
+    };
+    //Make a request
+    request.open("GET","http://kshitijsrivastava.imad.hasura-app.io/submit-name?name="+ name,true);
+    request.send(null);
+    
+   
 };
 
 
