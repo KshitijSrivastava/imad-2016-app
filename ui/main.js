30849c07a78dabe1ce8d05360aeaead48f3f672d
KshitijSrivastava <kshitij127@yahoo.co.in>
2016-11-11 00:08:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -70,7 +70,7 @@ submit.onclick=function(){
     //Make a request
     request.open("POST","http://kshitijsrivastava.imad.hasura-app.io/login",true);
     request.setRequestHeader('Content-Type','application/json');
-    request.send(JSON.stringify({username:"username",password:"password"}));
+    request.send(JSON.stringify({username:username,password:password}));
 };
 
 /*
