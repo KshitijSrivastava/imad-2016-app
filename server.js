2987844589c4e3e82e0f89ae22c0fa2244d61480
KshitijSrivastava <kshitij127@yahoo.co.in>
2016-11-10 16:41:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -128,7 +128,7 @@ app.get('/articles/:articleName',function(req,res)
    // var articleName=req.params.articleName; //extracting a parameter
     
 //SELECT * FROM article WHERE title='article-one'    (thinking as minus operator)
- pool.query("SELECT * FROM article WHERE title= '"+req.params.articleName+"'",function(err,result){
+ pool.query("SELECT * FROM article WHERE title= $1",[req.params.articleName],function(err,result){
      if(err)
      {
          res.status(500).send(err.toString());
