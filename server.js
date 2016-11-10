e0d0811567ab919e4c81052c926f3fa7d6c509e3
KshitijSrivastava <kshitij127@yahoo.co.in>
2016-11-10 13:17:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -121,12 +121,27 @@ app.get('/submit-name/',function(req,res){ //URL: /submit-name?name=xxxx
     res.send(JSON.stringify(result));
 });
 
-app.get('/:articleName',function(req,res)
+app.get('/articles/:articleName',function(req,res)
 {
     //articleName==article-one
     //articles[articleName]=article-one
-    var articleName=req.params.articleName; //extracting a parameter
-    res.send(createTemplate(articles[articleName]));
+   // var articleName=req.params.articleName; //extracting a parameter
+    
+ pool.query("SELECT * FROM article WHERE title="+req.params.articleName,function(err,result){
+     if(err)
+     {
+         res.status(500).send(err.toString());
+     }
+     else{
+         if(result.rows.length===0){
+             res.status(404).send('Article Not Found');
+         }
+         else{
+             var articleData=result.rows[0];
+              res.send(createTemplate(articleData));
+         }
+     }
+ });
 });
 
 app.get('/ui/style.css', function (req, res) {
