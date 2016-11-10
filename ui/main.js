c0dc7b2b3d36e481c31eaebf5d285b670f94bf51
KshitijSrivastava <kshitij127@yahoo.co.in>
2016-11-10 23:35:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,6 +15,7 @@
   //  var interval=setInterval(moveRight,50);
 //}
 
+/*
 //counter code
 var button=document.getElementById("counter"); 
 
@@ -38,10 +39,42 @@ button.onclick=function(){
     request.open("GET","http://kshitijsrivastava.imad.hasura-app.io/counter/",true);
     request.send(null);
 };
+*/
 
 
 var submit=document.getElementById("submit_btn");
+submit.onclick=function(){
+    // make a request to the server and send the name
+   //create a request object
+    var request= new XMLHttpRequest();
+    
+    //capture response and store it
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            //take some action
+            if(request.status===200){
+                console.log("user logged in");
+                alert("Logged in Succesfully");
+            }else if(request.status===403) {
+                alert("The Username?password is incorrect");
+            }else if(request.status===500) {
+                alert("Something Went Wrong on the Server");
+            }
+        }
+        // not done yet
+    };
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+    console.log(username);
+    console.log(password);
+    //Make a request
+    request.open("POST","http://kshitijsrivastava.imad.hasura-app.io/login",true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username:"username",password:"password"}));
+};
 
+/*
+var submit=document.getElementById("submit_btn");
 submit.onclick=function(){
     // make a request to the server and send the name
    //create a request object
@@ -71,10 +104,8 @@ submit.onclick=function(){
     //Make a request
     request.open("GET","http://kshitijsrivastava.imad.hasura-app.io/submit-name?name="+ name,true);
     request.send(null);
-    
-   
 };
-
+*/
 
 
 
