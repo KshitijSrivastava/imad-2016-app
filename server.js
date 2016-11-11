257a01e57e6fdfead38044338d927bb79c09aeef
KshitijSrivastava <kshitij127@yahoo.co.in>
2016-11-11 11:11:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
-var session = require('express-session');
+//var session = require('express-session');
 
 var config={
     user:"kshitijsrivastava",
@@ -17,10 +17,10 @@ var config={
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
-app.use(session({
+/*app.use(session({
     secret:"someRandomSecretValue",
     cookie:{ maxAge:1000*60*60*24*30}
-}));
+})); */
 
 function createTemplate(data)
 {
@@ -113,7 +113,7 @@ app.post('/login',function(req,res){
                if(hashedPassword===dbString){
                    
                       //Set a session
-                     req.session.auth={userId:results.row[0].id};
+                //     req.session.auth={userId:results.row[0].id};
                       //set a cookie with a session id
                       //internally on the server side,it maps the session id to an object
                       //{auth:{userId}}
