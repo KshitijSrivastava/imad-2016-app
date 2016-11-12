e605521ce96f8ddf16cdc455ef3616a1409750e3
KshitijSrivastava <kshitij127@yahoo.co.in>
2016-11-12 23:44:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -73,37 +73,6 @@ submit.onclick=function(){
     request.send(JSON.stringify({username:username,password:password}));
 };
 
-var register=document.getElementById("register_btn");
-register.onclick=function(){
-    // make a request to the server and send the name
-   //create a request object
-    var request= new XMLHttpRequest();
-    
-    //capture response and store it
-    request.onreadystatechange=function(){
-        if(request.readyState===XMLHttpRequest.DONE){
-            //take some action
-            if(request.status===200){
-                console.log("user is registered");
-                alert("Logged in Succesfully");
-            }else if(request.status===403) {
-                alert("The Username/password is incorrect");
-            }else if(request.status===500) {
-                alert("Something Went Wrong on the Server");
-            }
-        }
-        // not done yet
-    };
-    var username=document.getElementById("username").value;
-    var password=document.getElementById("password").value;
-    console.log(username);
-    console.log(password);
-    //Make a request
-    request.open("POST","http://kshitijsrivastava.imad.hasura-app.io/create-user",true);
-    request.setRequestHeader('Content-Type','application/json');
-    request.send(JSON.stringify({username:username,password:password}));
-};
-
 /*
 var submit=document.getElementById("submit_btn");
 submit.onclick=function(){
