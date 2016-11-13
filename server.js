991a51e6885e94646590ece212721dd165d282cf
KshitijSrivastava <kshitij127@yahoo.co.in>
2016-11-13 15:44:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -160,7 +160,7 @@ app.get('/get-articles',function(req,res){
          if(err){
            res.status(500).send(err.toString());
        } else{
-                res.send(JSON.stringify(result.rows));
+                res.send(JSON.stringify(result.row[0]));
                }
        });
 });
