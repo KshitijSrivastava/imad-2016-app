854159cbe1a31cf5adbd8c4d1bdfc17a3c925ccc
KshitijSrivastava <kshitij127@yahoo.co.in>
2016-11-18 11:58:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -61,32 +61,7 @@ function createTemplate(data)
             </div>
         </div>
         <script>
-        var comment_btn=document.getElementById("comment_btn");
-        comment_btn.onclick()=function(){
         
-            var request= new XMLHttpRequest();
-    
-    request.onreadystatechange=function(){
-        if(request.readyState===XMLHttpRequest.DONE){
-        
-            if(request.status===200){
-                console.log("user logged in");
-                alert("You have commented on the article");
-            }else {
-                alert("Something Went Wrong on the Server");
-            }
-        }
-        // not done yet
-    };
-    var comments=document.getElementById("comments").value;
-    
-    console.log(comments);
-    //Make a request
-    request.open("POST","http://kshitijsrivastava.imad.hasura-app.io/comment",true);
-    request.setRequestHeader('Content-Type','application/json');
-    request.send(JSON.stringify({article_id:1,user_id:"kshitij",comment:comments}));
-            
-        };
         </script>
     </body>
 </html>
