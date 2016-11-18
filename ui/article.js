e4ca7cc525d8da544b3a273e83b0c01645034111
KshitijSrivastava <kshitij127@yahoo.co.in>
2016-11-18 12:02:30 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -23,4 +23,4 @@ var comment_btn=document.getElementById("comment_btn");
     request.setRequestHeader('Content-Type','application/json');
     request.send(JSON.stringify({article_id:1,user_id:"kshitij",comment:comments}));
             
-        };
+        }
